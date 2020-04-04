64a11a3e2ae80ebc3f23151114e0634854e25dc9
kuljitkaur27 <kuljitkaur1984@gmail.com>
2020-04-04 16:57:34 +1100

    Version 1.1

--- new_backend_rails/market_backend/db/schema.rb
+++ new_backend_rails/market_backend/db/schema.rb
@@ -10,3 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_04_04_052135) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "password_digest", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
