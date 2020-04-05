3a63b5409ec6dc50814b24963a37daa3e1103219
leonardo souza <leo.aus88@gmail.com>
2020-04-05 13:28:02 +1000

    deleted front end folder

--- new_backend_rails/market_backend/db/schema.rb
+++ new_backend_rails/market_backend/db/schema.rb
@@ -16,15 +16,15 @@ ActiveRecord::Schema.define(version: 2020_04_04_110649) do
   enable_extension "plpgsql"
 
   create_table "branches", force: :cascade do |t|
-    t.text "branch_name"
-    t.text "location"
+    t.text "branch_name", null: false
+    t.text "location", null: false
     t.text "email"
     t.string "contact"
-    t.integer "user_id"
+    t.integer "user_id", null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "item"
+    t.string "item", null: false
     t.float "price"
     t.text "image"
   end
@@ -37,10 +37,9 @@ ActiveRecord::Schema.define(version: 2020_04_04_110649) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.text "email"
-    t.string "password_digest"
-    t.boolean "admin", default: false
+    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "password_digest", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
