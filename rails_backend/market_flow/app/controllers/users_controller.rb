b52d13ee63e59489fac091ffb9bfd40ee327daf9
kuljitkaur27 <kuljitkaur1984@gmail.com>
2020-04-06 20:02:48 +1000

    Version 1.7

--- rails_backend/market_flow/app/controllers/users_controller.rb
+++ rails_backend/market_flow/app/controllers/users_controller.rb
@@ -1,32 +1,40 @@
 class UsersController < ApplicationController
-  before_action :check_for_admin, :only => [:index]  #call before action
+  # before_action :check_for_admin, :only => [:index]  #call before action
 
-# GET /users.json
+
 #Index
   def index
-    @users = User.all
-    # format.json { render json: @users }
+    @users = User.where(nil)
+    filtering_params(params).each do |key, value|
+        @users = @users.public_send("filter_by_#{key}", value.capitalize) if value.present?
+      end
   end
 
-# #New User
-#   def new
-#     @user = User.new
-#   end
-#
-# #Create User
-#   def create
-#     @user = User.new user_params
-#     if @user.save
-#       session[:user_id] = @user.id
-#       redirect_to
-#     else
-#       render :new
-#     end
-#   end
+#New User
+  def new
+    # @user = User.new
+  end
+
+#Create User
+  def create
+    # @user = User.new user_params
+    # if @user.save
+    #   session[:user_id] = @user.id
+    #   redirect_to
+    # else
+    #   render :new
+    # end
+  end
+
 
 #Private Methods
   private
   # def user_params
-  #   params.require(:user).permit(:email, :password, :password_confirmation)
+  #   params.require(:user).permit(:name, :email, :password, :password_confirmation)
   # end
+
+    def filtering_params(params)
+      params.slice(:id, :email, :password, :admin)
+  end
+
 end
